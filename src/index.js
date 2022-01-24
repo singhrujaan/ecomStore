b9a3f89e67767fab9bc77b333cd81c587a864a52
Rujaan <singhrujaanman02@gmail.com>
2022-01-24 20:07:21 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import {store} from './redux/store'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/font-awesome/css/fonst-awesome.min.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Provider store={store}>
+  <App />
+  </Provider>
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
 
